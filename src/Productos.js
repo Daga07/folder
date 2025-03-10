13e007e554bc5a07659c65621f57f2e44046b280
Daga07 <danielga199902@gmail.com>
2025-03-09 22:30:58 -0500

    mejoras

--- src/Productos.js
+++ src/Productos.js
@@ -1,93 +1,76 @@
-import './Productos.css'
-import Pulceras from './Imagenes/pus.png';
+import React, { useState } from 'react';
+import './Productos.css';
+import Heraderimg from './Imagenes/pus.png';
+import Heraderimg1 from './Imagenes/instagram.png';
+import Heraderimg2 from './Imagenes/whatsapp.png';
+
+const productos = [
+    {   
+        id: 1,
+        imagenes: [Heraderimg, Heraderimg1, Heraderimg2],
+        precio: "$10.000",
+        disponible: "5 unidades",
+    },
+    {
+        id: 2,
+        imagenes: [Heraderimg, Heraderimg1, Heraderimg2],
+        precio: "$15.000",
+        disponible: "3 unidades",
+    },
+    {
+        id: 3, imagenes: [Heraderimg, Heraderimg1, Heraderimg2],
+        precio: "$12.000",
+        disponible: "7 unidades",
+    },
+    {
+        id: 4, imagenes: [Heraderimg, Heraderimg1, Heraderimg2],
+        precio: "$20.000",
+        disponible: "2 unidades",
+    }
+];
+
 function Productos() {
+    const [indices, setIndices] = useState(Array(productos.length).fill(0));
+
+    const cambiarImagen = (direccion, index) => {
+        setIndices(prevIndices => {
+            const nuevoIndex = (prevIndices[index] + direccion + productos[index].imagenes.length) % productos[index].imagenes.length;
+            const nuevosIndices = [...prevIndices];
+            nuevosIndices[index] = nuevoIndex;
+            return nuevosIndices;
+        });
+    };
+
     return (
-        <div className='ConProducto'>
-            <div className='Contec'>
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro' alt="Producto destacado"/>
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro'alt="Producto destacado" />
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro'alt="Producto destacado" />
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
-                
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro' alt="Producto destacado"/>
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro' alt="Producto destacado"/>
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro'alt="Producto destacado" />
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
-                <div className='Cuadros'>
-                    <img src={Pulceras} className='ImagenPro' alt="Producto destacado"/>
-                    <div className='PrecioPro'>Pulsera Confia</div>
-                    <div className='CantidadPro'>1000$</div>
-                </div>
+        <div className="ConProducto">
+            <div className="Contec">
+                {productos.map((producto, index) => (
+                    <div className="Cuadros" key={producto.id}>
+                        <div className="Carrusel">
+                            <img className="ImagenPro" src={producto.imagenes[indices[index]]} alt={`Producto ${producto.id}`} />
+                            <button className="BotonCarrusel BotonIzq" onClick={() => cambiarImagen(-1, index)}>❮</button>
+                            <button className="BotonCarrusel BotonDer" onClick={() => cambiarImagen(1, index)}>❯</button>
+                        </div>
+                        <p className="PrecioPro">{producto.precio}</p>
+                        <p className="CantidadPro">Disponible: {producto.disponible}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="Contec">
+                {productos.map((producto, index) => (
+                    <div className="Cuadros" key={producto.id}>
+                        <div className="Carrusel">
+                            <img className="ImagenPro" src={producto.imagenes[indices[index]]} alt={`Producto ${producto.id}`} />
+                            <button className="BotonCarrusel BotonIzq" onClick={() => cambiarImagen(-1, index)}>❮</button>
+                            <button className="BotonCarrusel BotonDer" onClick={() => cambiarImagen(1, index)}>❯</button>
+                        </div>
+                        <p className="PrecioPro">{producto.precio}</p>
+                        <p className="CantidadPro">Disponible: {producto.disponible}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
 }
-export default Productos
 
-
-
-/*
-
- 
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Pulsera Vida</div>
-                <div className='CantidadPro'>2000$</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Pulsera Amor</div>
-                <div className='CantidadPro'>3935$</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Precio= 10000$</div>
-                <div className='CantidadPro'>Cantidad 2</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Precio= 10000$</div>
-                <div className='CantidadPro'>Cantidad 2</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Precio= 10000$</div>
-                <div className='CantidadPro'>Cantidad 2</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Precio= 10000$</div>
-                <div className='CantidadPro'>Cantidad 2</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Precio= 10000$</div>
-                <div className='CantidadPro'>Cantidad 2</div>
-            </div>
-            <div>
-            <img src={Pulceras}className='ImagenPro'/> 
-                <div className='PrecioPro'>Precio= 10000$</div>
-                <div className='CantidadPro'>Cantidad 2</div>
-            </div>*/
+export default Productos;
