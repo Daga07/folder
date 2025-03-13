5c6763f38a5fb06d1f609f02b41ed112e4a2da9d
Daga07 <danielga199902@gmail.com>
2025-03-12 21:47:53 -0500

    Mensaje de whatsApp

--- src/Redes.js
+++ src/Redes.js
@@ -1,11 +1,17 @@
 import './Redes.css'
 import Logowhatsapp from './Imagenes/whatsapp.png';
 import Logoinstagram from './Imagenes/instagram.png';
+
+const numeroWhatsApp = "+573145296502";
+const mensaje = "Hola, estoy interesado en comprar una pulsera. ¿Podrías darme más información?";
+const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensaje)}`;
+
 function Redes(){
     return(
         <div className='flotante'> 
-        <a href="https://WhatsApp.com" target="_blank" rel="noopener noreferrer">
-        <img src={Logowhatsapp} alt="WhatsApp" className='iconI' /></a>
+        <a href={enlaceWhatsApp} target="_blank" rel="noopener noreferrer">
+        <img src={Logowhatsapp} alt="WhatsApp" className='iconI' />
+        </a>
         
         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
         <img src={Logoinstagram} alt="instagram" className='iconI' /> </a>
