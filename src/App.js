3315aee907ceb87e68e745176e51e30d19d40e95
Daga07 <danielga199902@gmail.com>
2025-03-22 20:41:31 -0500

    Titulo Nuevo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Productos from './Productos';
 import Redes from './Redes';
 import Heraderimg from './Imagenes/pus.png';
+
 function App() {
   return (
     <div className="App">
       <header className='header'>
         <div className='header-content'>
         <img src={Heraderimg} className='herader-img' alt="Producto destacado"/>
-        <h1 className='header-titu'> Manillas y accesosrios</h1>
+        <h1 className='header-titu'>Lux jewels</h1>
         </div>
       </header>
       <Productos />
